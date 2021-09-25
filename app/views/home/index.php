cfcd348c11b55e60a554527a67dd79e85a90daa2
IsRylo <91057829+IsRylo@users.noreply.github.com>
2021-09-25 23:27:14 +0800

    Update index.php
    Add image

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,6 +4,6 @@
 <iframe width="560" height="315" src="https://www.youtube.com/embed/V6xmFJmbL94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <h1>Tidak ada ide!</h1>
 
-<img src="https://yt3.ggpht.com/ytc/AKedOLTq7Wg8iRjcwhTAuiAnFY4aVHC12t-evvLXj7Ny=s176-c-k-c0x00ffffff-no-rj">
+<img src="https://cdn0-production-images-kly.akamaized.net/tkhvPD4wU7y3pyTzuUNd6ODdMz0=/640x640/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/1141241/original/001968500_1455438595-chi-success-kid-meme-dad-kidney.jpg">
 
 
