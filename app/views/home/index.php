a1f7a5adbe77cf6ac895401fa6892587be868fe3
AlfredPros <alfredkuhlman@yahoo.co.id>
2021-09-25 22:10:15 +0700

    test

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,2 +1,4 @@
 <h1>Welcome to mvc :)</h1>
 <a class="btn btn-primary" href="<?= base_url ?>/phone">Go to crud phone</a>
+<br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/V6xmFJmbL94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
