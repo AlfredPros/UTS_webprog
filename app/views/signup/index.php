24ecd5f4d00f7666100b8ecbb7b33f19eed375f6
IsRylo <91057829+IsRylo@users.noreply.github.com>
2021-09-26 03:24:40 +0800

    Button
    Butoton

--- app/views/signup/index.php
+++ app/views/signup/index.php
@@ -24,12 +24,19 @@
                 <input type="date" class="form-control" id="birthdate" name="birthdate" placeholder="2900-9-21" value="" required>
             </div>
             <div class="form-group" style="padding-bottom:15px">
-                <label for="gender">Jenis Kelamin</label>
-                <input type="text" class="form-control" id="gender" name="gender" placeholder="M/F" value="" required>
+                <label for="gender">Jenis Kelamin</label><br>
+                <input class="form-control" list = "genderList" type="text" id="gender" placeholder= "Male/Female" name="" required>
+                <datalist id="genderList" name="gender" value="" >
+                    <option value="Male">
+                    <option value="Female">
+                    <option value="Other">
+                </datalist>
             </div>
             <div class="form-group" style="padding-bottom:15px">
                 <label for="photo">Foto Profil</label>
-                <input type="text" class="form-control" id="photo" name="photo" placeholder="Your Face" value="" required>
+                <div class="custom-file">
+                    <input type="file" class="form-control" id="photo" name="photo" placeholder="Your Face" value="" required>
+                </div>
             </div>
 
             <button type="submit" class="btn btn-primary" style="margin-right:5px">Submit</button>
