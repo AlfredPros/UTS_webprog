1838e8d6323d2a101fff4a3e3cfcb81985a6f92c
IsRylo <91057829+IsRylo@users.noreply.github.com>
2021-09-25 23:16:50 +0800

    Update index.php
    Mauskkan input

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,3 +3,4 @@
 <br>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/V6xmFJmbL94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <h1>Tidak ada ide!</h1>
+<input type="text" placeholder="Hello">
