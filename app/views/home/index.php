916ab4cd02f998059c2aa6d763c7e9fb71878b55
IsRylo <91057829+IsRylo@users.noreply.github.com>
2021-09-26 01:04:49 +0800

    Update index.php
    Fix stupid mistake

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,7 +3,7 @@
             for ($i = 0; $i < 3; $i++)
             {
                 ?>
-                <div class="row justify content-md-center">
+                <div class="row">
                     <div class="col-md-4">
                         <div class="row">
                             <img src="http://localhost/UTS_webprog/design/home_notlogin.png" alt="" >
@@ -12,12 +12,36 @@
                             <div class="col">
                                 <p style="font-size: large;">Judul</p>
                             </div>
+                        </div>
+                        <div class="row">
                             <div class="col">
-                                <p style="font-size: medium; margin: right 16px;">Penulis</p>
+                                <p style="font-size: medium;">Penulis</p>
+                            </div>
+                            <div class="col">
+                                <p style="font-size: medium;">Tanggal Terbit</p>
                             </div>
                         </div>
                         <div class="row">
-                            <p style="font-size: medium;">Tanggal Terbit</p>
+                            <p style="font-size: small;">Kategori</p>
+                        </div>
+                    </div>
+                        
+                    <div class="col-md-4">
+                        <div class="row">
+                            <img src="http://localhost/UTS_webprog/design/home_notlogin.png" alt="" >
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <p style="font-size: large;">Judul</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <p style="font-size: medium;">Penulis</p>
+                            </div>
+                            <div class="col">
+                                <p style="font-size: medium;">Tanggal Terbit</p>
+                            </div>
                         </div>
                         <div class="row">
                             <p style="font-size: small;">Kategori</p>
@@ -32,39 +56,21 @@
                             <div class="col">
                                 <p style="font-size: large;">Judul</p>
                             </div>
-                            <div class="col">
-                                <p style="font-size: medium; margin: right 16px;">Penulis</p>
-                            </div>
                         </div>
                         <div class="row">
-                            <p style="font-size: medium;">Tanggal Terbit</p>
+                            <div class="col">
+                                <p style="font-size: medium;">Penulis</p>
+                            </div>
+                            <div class="col">
+                                <p style="font-size: medium;">Tanggal Terbit</p>
+                            </div>
                         </div>
                         <div class="row">
                             <p style="font-size: small;">Kategori</p>
                         </div>
                     </div>
-
-                    <div class="col-md-4">
-                        <div class="row">
-                            <img src="http://localhost/UTS_webprog/design/home_notlogin.png" alt="">
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <p style="font-size: large;">Judul</p>
-                            </div>
-                            <div class="col">
-                                <p style="font-size: medium; margin: right 16px;">Penulis</p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <p style="font-size: medium;">Tanggal Terbit</p>
-                        </div>
-                        <div class="row">
-                            <p style="font-size: small;">Kategori</p>
-                        </div>
-                    </div>
-                    
-                </div> <?php
+                </div>
+                 <?php
         };
         ?>
 </div>
