85b707aac3eaaa6d6d46d1e26af9ea1b1ee8e8f6
IsRylo <91057829+IsRylo@users.noreply.github.com>
2021-09-26 00:52:53 +0800

    Table?
    Table rusak? yes

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,70 @@
-<h1>hello there</h1>
+<div class="container">
+        <?php 
+            for ($i = 0; $i < 3; $i++)
+            {
+                ?>
+                <div class="row justify content-md-center">
+                    <div class="col-md-4">
+                        <div class="row">
+                            <img src="http://localhost/UTS_webprog/design/home_notlogin.png" alt="" >
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <p style="font-size: large;">Judul</p>
+                            </div>
+                            <div class="col">
+                                <p style="font-size: medium; margin: right 16px;">Penulis</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <p style="font-size: medium;">Tanggal Terbit</p>
+                        </div>
+                        <div class="row">
+                            <p style="font-size: small;">Kategori</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="row">
+                            <img src="http://localhost/UTS_webprog/design/home_notlogin.png" alt="" >
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <p style="font-size: large;">Judul</p>
+                            </div>
+                            <div class="col">
+                                <p style="font-size: medium; margin: right 16px;">Penulis</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <p style="font-size: medium;">Tanggal Terbit</p>
+                        </div>
+                        <div class="row">
+                            <p style="font-size: small;">Kategori</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="row">
+                            <img src="http://localhost/UTS_webprog/design/home_notlogin.png" alt="">
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <p style="font-size: large;">Judul</p>
+                            </div>
+                            <div class="col">
+                                <p style="font-size: medium; margin: right 16px;">Penulis</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <p style="font-size: medium;">Tanggal Terbit</p>
+                        </div>
+                        <div class="row">
+                            <p style="font-size: small;">Kategori</p>
+                        </div>
+                    </div>
+                    
+                </div> <?php
+        };
+        ?>
+</div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -31,7 +31,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#F1F4F5">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Berita Anime</a>
+            <a class="navbar-brand" href=<?= 'http://localhost/UTS_webprog/public/' ?> style="">Berita Anime</a>
             <!-- Button for smaller-size -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
