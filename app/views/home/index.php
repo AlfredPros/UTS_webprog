0376a6283f48f512231775b2a89488557f420e4d
AlfredPros <alfredkuhlman@yahoo.co.id>
2021-09-25 22:16:15 +0700

    img
    smg

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,3 +3,4 @@
 <br>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/V6xmFJmbL94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <h1>Tidak ada ide!</h1>
+<img src="https://yt3.ggpht.com/ytc/AKedOLTq7Wg8iRjcwhTAuiAnFY4aVHC12t-evvLXj7Ny=s176-c-k-c0x00ffffff-no-rj">
