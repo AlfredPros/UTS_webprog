4dbe25eead5e3167f60ad13863567c8cd0324a1e
Adhitya Bagus Wicaksono <adhityabagus80@gmail.com>
2021-10-03 03:30:34 +0700

    Perbaikan Minor

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,7 +1,21 @@
 <!--<!DOCTYPE html>
 <html lang="en">
+    <head>
+        <style>
+            footer {
+                position: fixed;
+                width: 100%;
+                bottom: 10px;
+                margin-top: 15px;
+                padding: 15px;
+                text-align: center;
+                background-color: #C90000;
+                color: white;
+            }
+        </style>
+    </head>
     <body>
-        <footer style="margin-top: 15px; padding: 15px; text-align: center; background-color: #C90000; color: white">
+        <footer>
             <p><b>Dibuat dengan Penuh Cinta oleh Kelompok Kawai-Desu</b></p>
             <img src="http://localhost/UTS_webprog/design/AnimeNetworkLogo.png" alt="" width="200">
         </footer>
