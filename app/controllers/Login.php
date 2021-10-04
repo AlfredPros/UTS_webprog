2eb81f703270b64d63b0778e2a9025fa5dabdf47
AlfredPros <alfredkuhlman@yahoo.co.id>
2021-10-04 13:38:18 +0700

    Tiny optimization
    While -> Do while

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -2,26 +2,18 @@
 
 class Login extends Controller
 {
-	public function __construct()
-	{
-		// if (!isset($_SESSION['session_login']) && $_SESSION['session_login'] != 'sudah_login') {
-		// 	echo 'Login Tidak ditemukan. danger';
-		// 	header('location: ' . base_url . '/login');
-		// 	exit;
-		// }
-	}
 	public function index()
 	{
 		function randString($length = 7) {
 			$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 			$charactersLength = strlen($characters);
-			$randomString = '';
-			while(!preg_match('~[0-9]+~', $randomString)) {
+			do {
 				$randomString = '';
 				for ($i = 0; $i < $length; $i++) {
 					$randomString .= $characters[rand(0, $charactersLength - 1)];
 				}
-			};
+			}
+			while (!preg_match('~[0-9]+~', $randomString));
 			return $randomString;
 		}
 
