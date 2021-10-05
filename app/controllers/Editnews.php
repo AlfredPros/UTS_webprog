ce4c00de0bf352abecae6c006df7528dc7bfc3f4
AlfredPros <alfredkuhlman@yahoo.co.id>
2021-10-05 14:12:10 +0700

    some1coolbro

--- app/controllers/Editnews.php
+++ app/controllers/Editnews.php
@@ -14,6 +14,12 @@ class Editnews extends Controller
 	{
 		$data['title'] = 'Edit Berita';
 
+		$url = rtrim($_GET['url'], '/');
+		$urlclean = filter_var($url, FILTER_SANITIZE_URL);
+		$urlget = explode('/', $urlclean);
+
+		$data['nid'] = $this->model('NewsModel')->getNews($urlget[1]);
+
 		$this->view('templates/header', $data);
 		$this->view('editnews/index', $data);
 		$this->view('templates/footer');
