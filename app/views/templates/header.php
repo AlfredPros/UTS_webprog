d37868f3e089359494bbc0f4de627b933b8b48ce
Adhitya Bagus Wicaksono <adhityabagus80@gmail.com>
2021-10-06 13:26:19 +0700

    Menambah Ikon Tab
    Ikon tab menjadi logo Anime News

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= $data['title'] ?> </title>
+    <link rel="icon" href="http://localhost/UTS_webprog/design/anime_news_icon.png">
 
     <!-- Bootstrap and jQuery -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
